4fce6eab80e65fdb9a19aa010839123a4336eea1
Ferran Homet Puigbo <ferran.homet@gmail.com>
2016-03-29 01:33:31 +0200

    Script templates

---
+++ plantilles.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python
+#-*- coding: utf-8-*-
+
+ADDUSER = """dn: cn=%s,ou=usuaris,dc=edt,dc=org
+objectclass: posixAccount
+objectclass: inetOrgPerson
+cn: %s
+sn: usuari
+ou: usuaris
+uid: %s
+uidNumber: %s
+gidNumber: %s
+homeDirectory: %s
+
+"""
+
+ADDGROUP="""dn: cn=%s,ou=groups,dc=edt,dc=org
+objectclass: posixGroup
+objectclass: top
+cn: %s
+gidNumber: %s
+description: Container per al grup unix %s
+"""
+
+
+PASSWD = """%s:x:%s:%s:%s
+"""
+GROUP = """%s:x:%s:%s
+"""
