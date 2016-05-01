f7c8ff41fc6ea14a6f94780a357fda823ad4af6d
Ferran Homet Puigbo <ferran.homet@gmail.com>
2016-05-02 01:51:58 +0200

    V1.2

--- pract1.py
+++ pract1.py
@@ -4,7 +4,7 @@
 #Exercici 1: client ldap
 # Escola del treball de Barcelona 
 # ASIX Hisi2 M06­ASO UF2NF1­Scripts 
-# @edt Curs 20152016  gener 2016
+# @edt Curs 20152016  abril 2016
 # Ferran Homet Puigbo
 # isx47167139
 
@@ -33,13 +33,21 @@ class UnixGroup():
 	"Classe que defixeix un POSIX group Unix"
 	def __init__(self,groupLine):
 		"Constructor de grup donada una línia"
+		if groupLine[-1] == '\n':
+			groupLine = groupLine[:-1]
 		groupFields=groupLine.split(':')
 		self.gname=groupFields[0]
 		self.password=groupFields[1]
 		self.gid=groupFields[2]
+		# Comprovar si hi ha usuaris amb el grup secundari
 		if groupFields[3]=='':
+			# si no en te, deixem la llista buida
 			self.userList=[]
+		# Sino, inserim els usuaris
 		else:
+			# Treure \n del final de linia
+			if groupFields[3][-1] == '\n':
+				groupFields[3] = groupFields[3][:-1]
 			self.userList=groupFields[3].split(',') 
 	def __str__(self):
 		return "%15s:%5d:%s" % \
@@ -49,21 +57,19 @@ class UnixGroup():
 class UnixUser():
 	"Classe que defixeix un usuari POSIX Unix"
 	def __init__(self,userLine):
-		"Constructor d'usuaris donada una línia"
 		userFields=userLine.split(':')
+		self.login=userFields[0]
+		self.password=userFields[1]
+		self.uid=userFields[2]
+		self.gid=userFields[3]
+		self.gecos=userFields[4]
+		self.homeDirectory=userFields[5]
+		self.shell=userFields[6]
+		# Comprobar que existeixi el grup del usuari
 		if dicGroups.has_key(self.gid):
-			self.login=userFields[0]
-			self.password=userFields[1]
-			self.uid=userFields[2]
-			self.gid=userFields[3]
-			self.gecos=userFields[4]
-			self.homeDirectory=userFields[5]
-			self.shell=userFields[6]
 			self.gname=dicGroups[self.gid].gname
 		else:
-			sys.stdout.write("El usuari %s No pertany a cap grup! .. ERROR!\n" % login)
-			pass
-			#b) sys.stderror,info user i gname ERROR, and pass
+			print 'Grup inexistent: %s' % (self.gid)
 	def __str__(self):
 		return "{%s:%s:%s:%s:%s:%s:%s}" % \
 		(self.login,self.uid,self.gid,self.gname,self.gecos,self.homeDirectory,self.shell)
@@ -75,6 +81,14 @@ if __name__ == '__main__':
 	#	sys.stderr.write('mala suerte')
 	#	sys.exit(nananianonanianonaniano)
 	
+	fitxer = 'etc.ldif'
+	# Comprobem que poguem crear el fitxer per guardar les dades
+	try:
+		ff = open(fitxer, 'wb')
+	except:
+		sys.stderr.write("Error al crear el fitxer\n")
+		sys.exit(2)
+	
 	# Carregar grups
 	# Crear diccionari grups
 	dicGroups={}
@@ -84,14 +98,6 @@ if __name__ == '__main__':
 	args.fileGroups.close()
 	
 	
-	fitxer = 'etc.ldif'
-	# Comprobem que poguem crear el fitxer per guardar les dades
-	try:
-		ff = open(fitxer, 'wb')
-	except:
-		sys.stderr.write("Error al crear el fitxer\n")
-		sys.exit(2)
-	
 	# Carregar usuaris
 	# Crear diccionari usuaris
 	# Escriure usuaris al fitxer ldif
@@ -99,12 +105,18 @@ if __name__ == '__main__':
 	for line in args.fileUsers:
 		oneUser=UnixUser(line[:-1])
 		dicUsers[oneUser.login]=oneUser
-		user = plantilles.ADDUSER % (oneUser.login,
-						  oneUser.login, 
-						  oneUser.login,
-						  oneUser.uid,
-						  oneUser.gid,
-						  oneUser.homeDirectory)
+		# Comprovar que existeixi el grup principal del usuari
+		if oneUser.gid in dicGroups:
+			# Afegir a la llista d'usuaris, aquells que el tinguin com a principal	
+			if oneUser.login not in dicGroups[oneUser.gid].userList
+				# Afegir a la llista d'usuaris del grup principal el login de l'usuari
+				oneUser.gid.userList.append(oneUser.login)
+				user = plantilles.ADDUSER % (oneUser.login,
+								  oneUser.login, 
+								  oneUser.login,
+								  oneUser.uid,
+								  oneUser.gid,
+								  oneUser.homeDirectory)
 		ff.write(user)
 	args.fileUsers.close()
 
@@ -112,25 +124,25 @@ if __name__ == '__main__':
 	# Creem una llista amb els usuaris amb grup secundari
 	# Guardem els groups en format ldif al fitxer
 	for group in dicGroups:
-		ll_membres=[]
+		memberin = ''
 		for login in dicGroups[group].userList:
-			ll_membres.append('memberUid: %s' % (dicUsers[login].uid +'\n'))
+			memberin += 'memberUid: %s\n' % (login)
 		group = plantilles.ADDGROUP % (dicGroups[group].gname,
 								dicGroups[group].gname, 
 								dicGroups[group].gid,
-								dicGroups[group].gname)
+								dicGroups[group].gname,
+								memberin)
 		ff.write(group)
-	if len(ll_membres) > 0:
-		for membre in ll_membres:
-			ff.write(membre)
 	ff.write('\n')
 	ff.close()
-	
-	# Popen per inserir les dades al servidor LDAP
-	LDAPADD = ["ldapadd", "-x","-h","localhost", "-D", "cn=Manager,dc=edt,dc=org", "-w", "jupiter", "-f", fitxer]
-	pipeData = Popen(LDAPADD, stdout=PIPE, stderr=PIPE)
+	try:
+		# Popen per inserir les dades al servidor LDAP
+		LDAPADD = ["ldapadd", "-x","-h","localhost", "-D", "cn=Manager,dc=edt,dc=org", "-w", "jupiter", "-f", fitxer]
+		pipeData = Popen(LDAPADD, stdout=PIPE, stderr=PIPE)
+	except:
+		sys.exit(3)
 	sys.exit(0)
-
+	
 # Comentaris del professor
 '''
 '''
